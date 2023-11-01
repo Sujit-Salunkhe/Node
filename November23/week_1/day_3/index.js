9a61f41bb4b4378c4e968e0233f29a29f9a2ea51
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-11-01 14:29:34 +0530

    using html in server

--- November23/week_1/day_3/index.js
+++ November23/week_1/day_3/index.js
@@ -1,12 +1,16 @@
 const http = require('node:http')
+const fs = require('node:fs')
 const createserver = http.createServer((req,res) =>{
     // console.log(res)
     const superhero ={
         firstName : "sujit",
         lastName : "salunkhe"
     }
-    res.writeHead(200,{"Content-Type" : "application/json"})
-    res.end(JSON.stringify(superhero))
+    res.writeHead(200,{"Content-Type" : "text/html"})
+    // res.end(JSON.stringify(superhero))
+    const html = fs.readFileSync('./index.html',"utf-8")
+    fs.createReadStream(__dirname + './index.html').pipe(res)
+    // res.end(html)
     // console.log(req)
 })
 
