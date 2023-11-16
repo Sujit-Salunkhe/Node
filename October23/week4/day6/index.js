3fbdbc48d6f8abbdde99fbcdf87612f8d67ee1d9
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-11-16 19:31:05 +0530

    learning clusters

--- October23/week4/day6/index.js
+++ October23/week4/day6/index.js
@@ -8,10 +8,3 @@ pizzShop.on('order', (size,topping) => {
 })
 pizzShop.orde("large","chessy");
 pizzShop.displyordernumber ();
-// const EventEmitter = require("node:events")
-// const emitter = new EventEmitter()
-// emitter.on("order-Pizza",(size,topping) => {
-//     console.log(`ordered receive baking a ${size} pizza with ${topping}`)
-// })
-// emitter.emit("order-Pizza","large","mushroom")
-// console.log("sujit")
--- October23/week5/day1/index.js
+++ October23/week5/day1/index.js
@@ -1,5 +1,5 @@
-// const { error } = require("node:console")
-// const { read } = require("node:fs")
+const { error } = require("node:console")
+const { read } = require("node:fs")
 
 const fs = require("node:fs")
 const zlib=require("node:zlib")
@@ -18,44 +18,46 @@ const writebleStream = fs.WriteStream('./file2.txt')
 
 readableStream.pipe(writebleStream) 
 
-// async function readFile () {
-//     try{
-//         const data = await fs.readFile("file.txt","utf-8")
-//         console.log(data)
-//     }catch(err) {
-//         console.log(err)
-//     }
-// }
-// readFile()
+async function readFile () {
+    try{
+        const data = await fs.readFile("file.txt","utf-8")
+        console.log(data)
+    }catch(err) {
+        console.log(err)
+    }
+}
+readFile()
 
-// console.log("first");
-// fs.readFile("file.txt","utf-8")
-// .then(data => console.log(data))
-// .catch(error => console.log(error));
+console.log("first");
+fs.readFile("file.txt","utf-8")
+.then(data => console.log(data))
+.catch(error => console.log(error));
 
-// console.log("Second");
+console.log("Second");
 
-// const { error } = require("node:console");
-// const { errorMonitor } = require("node:events");
-// const fs  = require("node:fs");
-// const fileContacts = fs.readFileSync("./file.txt","utf-8")
-// console.log("First")
-// console.log(fileContacts)
-// console.log("second")
-// fs.readFile('./file.txt',"utf-8" ,(error,data) => {
-//     if (error){
-//         console.log(error)
-//     }else {
-//         console.log(data)
-//     }
-// });
-// console.log("third")
 
-// fs.writeFileSync("./greet.txt","hello zandu");
-// fs.writeFile('./greet.txt'," hello balm",{flag: "a"},(error) => {
-//     if (error) {
-//         console.log(error)
-//     } else {
-//         console.log("file is written")
-//     }
-// })
+
+const { error } = require("node:console");
+const { errorMonitor } = require("node:events");
+const fs  = require("node:fs");
+const fileContacts = fs.readFileSync("./file.txt","utf-8")
+console.log("First")
+console.log(fileContacts)
+console.log("second")
+fs.readFile('./file.txt',"utf-8" ,(error,data) => {
+    if (error){
+        console.log(error)
+    }else {
+        console.log(data)
+    }
+});
+console.log("third")
+
+fs.writeFileSync("./greet.txt","hello zandu");
+fs.writeFile('./greet.txt'," hello balm",{flag: "a"},(error) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("file is written")
+    }
+})
