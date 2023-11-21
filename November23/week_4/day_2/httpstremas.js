d817d7fe6dad30063db32dd6679f841267f936a4
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-11-21 14:23:31 +0530

    learning httpstreams with servers

---
+++ November23/week_4/day_2/httpstremas.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const fs = require('fs')
+const { error } = require('console')
+http.createServer((req,res) => {
+    // const text = fs.readFileSync('./bigfile.txt','utf-8')
+    // res.end(text)
+    const text = fs.createReadStream('./bigfile.txt','utf8')
+    text.on('open', () =>{
+        text.pipe(res)
+    })
+    text.on('error',() =>{
+        res.end(error)
+    })
+}).listen(3000)
--- November23/week_4/day_2/index.js
+++ November23/week_4/day_2/index.js
@@ -1,2 +1,9 @@
-const EventEmitter = require("events")
+const EventEmitter = require("events");
 const custuomEmitter = new EventEmitter()
+custuomEmitter.on("response",() => {
+    console.log("your response is fullfilled")
+});
+custuomEmitter.on("response",() => {
+    console.log("yes theese event can be created a twise")
+});
+custuomEmitter.emit('response');
